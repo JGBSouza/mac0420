5bddf55e8604be718b483e73bec5341082e1eae6
JGBSouza <joaosouzaaa12@gmail.com>
2025-04-21 22:49:28 -0300

    add ep02 base
    Signed-off-by: JGBSouza <joaosouzaaa12@gmail.com>

---
+++ ep02/paredao.js
@@ -0,0 +1,50 @@
+/* ==================================================
+    EP02 - Exercício programa de MAC0420/MAC5744
+
+    Nome: João Guilherme Barbosa de Souza
+    NUSP: 12543850
+
+    Ao preencher esse cabeçalho com o meu nome e o meu número USP,
+    declaro que todas as partes originais desse exercício programa (EP)
+    foram desenvolvidas e implementadas por mim e que portanto não 
+    constituem desonestidade acadêmica ou plágio.
+    Declaro também que sou responsável por todas as cópias desse
+    programa e que não distribui ou facilitei a sua distribuição.
+    Estou ciente que os casos de plágio e desonestidade acadêmica
+    serão tratados segundo os critérios divulgados na página da 
+    disciplina.
+    Entendo que EPs sem assinatura devem receber nota zero e, ainda
+    assim, poderão ser punidos por desonestidade acadêmica.
+
+    Abaixo descreva qualquer ajuda que você recebeu para fazer este
+    EP.  Inclua qualquer ajuda recebida por pessoas (inclusive
+    monitores e colegas). Com exceção de material da disciplina, caso
+    você tenha utilizado alguma informação, trecho de código,...
+    indique esse fato abaixo para que o seu programa não seja
+    considerado plágio ou irregular.
+================================================== */
+
+/* CORES */
+const GREEN07 = [0.0, 0.7, 0.0, 1.0];
+const WHITE   = [1.0, 1.0, 1.0, 1.0];
+const BLACK   = [0.0, 0.0, 0.0, 1.0];
+const RED     = [1.0, 0.0, 0.0, 1.0];
+const GREEN   = [0.0, 1.0, 0.0, 1.0];
+const BLUE    = [0.0, 0.0, 1.0, 1.0];
+const MAGENTA = [1.0, 0.0, 1.0, 1.0];
+const YELLOW  = [1.0, 1.0, 0.0, 1.0];
+
+const COR_FUNDO   = [0.0, 0.25, 0.0, 1.0];
+const COR_RAQUETE = [0.3, 0.5, 1.0, 1.0];
+const COR_BOLA = YELLOW;
+const COR_TIJOLO  = MAGENTA;
+
+// ---------------------------------------------------
+// passo é o intervalo de tempo usado na depuração
+const PASSO = 0.10;  
+
+window.onload = main
+
+function main() {
+    
+}
